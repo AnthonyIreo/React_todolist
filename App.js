970e4b0392a1d27e281a455accc121ea82671238
AnthonyIreo <baosheng@buffalo.edu>
2022-12-02 15:47:33 -0500

    part1

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity} from 'react-native';
 
 import Task from './components/Task';
 
@@ -7,16 +7,30 @@ export default function App() {
   return (
     <View style={styles.container}>
     	
-	{/* Today's tasks */}
-	<View style={styles.tasksWrapper}>
-	  <Text style={styles.sectionTitle}>Today's Tasks</Text>
-	    <View style={styles.items}>
-	      <Task text={'Task 1'}/>
-	      <Task text={'Task 2'}/>
-	    </View>
-	</View>
+	  {/* Today's tasks */}
+	  <View style={styles.tasksWrapper}>
+	  <Text style={styles.sectionTitle}>Today's Tasks</Text> 
+    <View style={styles.items}> 
+    <Task text={'Task 1'}/> 
+    <Task text={'Task 2'}/> 
 
+    </View> 
 
+    </View>
+
+  {/* Write a task */}
+  <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding":"height"}
+      style={styles.writeTaskWrapper}
+      > 
+      <TextInput style={styles.input} placeholder={'Write a task'} />
+      
+      <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -37,4 +51,32 @@ const styles = StyleSheet.create({
   items: {
 	  marginTop: 30,
   },
+  writeTaskWrapper:{
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    
+  }
 });
